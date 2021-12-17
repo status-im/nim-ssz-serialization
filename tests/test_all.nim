9e7077768498ad7927bdf14b5a2ef39b4e5018c8
Etan Kissling <etan@status.im>
2021-12-17 17:03:00 +0100

    Integrate merkle proof code from `nimbus-eth2` (#19)
    This moves generic merkle proof related code from `nimbus-eth2`,
    notably enabling use of `is_valid_merkle_branch` by `nimbus-eth1`.

--- tests/test_all.nim
+++ tests/test_all.nim
@@ -1,5 +1,13 @@
+# ssz_serialization
+# Copyright (c) 2018, 2021 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
 import
   ./test_bitseqs,
+  ./test_proofs,
   ./test_ssz_roundtrip,
   ./test_ssz_serialization,
   ./test_ssz_union,
