status-im/nim-ssz-serialization

commit ab73136ce8598f27d218a44dca8f42493d7cd382

nimble/ci updates (#69)

ssz_serialization.nimble
@@ -16,17 +16,30 @@ requires "nim >= 1.6.0",
          "blscurve",
          "unittest2"
 
-proc test(args, path: string) =
-  if not dirExists "build":
-    mkDir "build"
-  exec "nim " & getEnv("TEST_LANG", "c") & " " & getEnv("NIMFLAGS") & " " & args &
-    " -r --skipParentCfg" &
-    " --styleCheck:usages --styleCheck:error --hint[Processing]:off " &
-    path
+let nimc = getEnv("NIMC", "nim") # Which nim compiler to use
+let lang = getEnv("NIMLANG", "c") # Which backend (c/cpp/js)
+let flags = getEnv("NIMFLAGS", "") # Extra flags for the compiler
+let verbose = getEnv("V", "") notin ["", "0"]
+
+from os import quoteShell
+
+let cfg =
+  " --styleCheck:usages --styleCheck:error" &
+  (if verbose: "" else: " --verbosity:0 --hints:off") &
+  " --skipParentCfg --skipUserCfg --outdir:build " &
+  quoteShell("--nimcache:build/nimcache/$projectName")
+
+proc build(args, path: string) =
+  exec nimc & " " & lang & " " & cfg & " " & flags & " " & args & " " & path
+
+proc run(args, path: string) =
+  build args & " -r", path
+  if (NimMajor, NimMinor) > (1, 6):
+    build args & " --mm:refc -r", path
 
 task test, "Run all tests":
-  test "--threads:off -d:PREFER_BLST_SHA256=false", "tests/test_all"
-  test "--threads:on -d:PREFER_BLST_SHA256=false", "tests/test_all"
+  run "--threads:off -d:PREFER_BLST_SHA256=false", "tests/test_all"
+  run "--threads:on -d:PREFER_BLST_SHA256=false", "tests/test_all"
   if (NimMajor, NimMinor) > (1, 6):
-    test "--mm:refc --threads:off -d:PREFER_BLST_SHA256=false", "tests/test_all"
-    test "--mm:refc --threads:on -d:PREFER_BLST_SHA256=false", "tests/test_all"
+    run "--mm:refc --threads:off -d:PREFER_BLST_SHA256=false", "tests/test_all"
+    run "--mm:refc --threads:on -d:PREFER_BLST_SHA256=false", "tests/test_all"

.github/workflows/ci.yml
@@ -6,6 +6,10 @@ on:
   pull_request:
   workflow_dispatch:
 
+concurrency: # Cancel stale PR builds (but not push builds)
+  group: ${{ github.workflow }}-${{ github.event.pull_request.number || github.sha }}
+  cancel-in-progress: true
+
 jobs:
   build:
     strategy:
@@ -43,6 +47,7 @@ jobs:
 
     name: '${{ matrix.target.os }}-${{ matrix.target.cpu }} (Nim ${{ matrix.branch }})'
     runs-on: ${{ matrix.builder }}
+    continue-on-error: ${{ matrix.branch == 'devel' }}
     steps:
       - name: Checkout
         uses: actions/checkout@v3
@@ -66,13 +71,6 @@ jobs:
           EOF
           chmod 755 external/bin/gcc external/bin/g++
           echo '${{ github.workspace }}/external/bin' >> $GITHUB_PATH
-
-      - name: 'Install dependencies (macOS)'
-        if: runner.os == 'macOS' && matrix.branch == 'devel'
-        run: |
-          brew install openssl@1.1
-          ln -s $(brew --prefix)/opt/openssl/lib/libcrypto.1.1.dylib /usr/local/lib
-          ln -s $(brew --prefix)/opt/openssl/lib/libssl.1.1.dylib /usr/local/lib/
 
       - name: MSYS2 (Windows i386)
         if: runner.os == 'Windows' && matrix.target.cpu == 'i386'
@@ -98,25 +96,25 @@ jobs:
       - name: Restore Nim DLLs dependencies (Windows) from cache
         if: runner.os == 'Windows'
         id: windows-dlls-cache
-        uses: actions/cache@v3
+        uses: actions/cache@v2
         with:
-          path: external/dlls
-          key: 'dlls'
+          path: external/dlls-${{ matrix.target.cpu }}
+          key: 'dlls-${{ matrix.target.cpu }}'
 
-      - name: Install DLL dependencies (Windows)
+      - name: Install DLLs dependencies (Windows)
         if: >
           steps.windows-dlls-cache.outputs.cache-hit != 'true' &&
           runner.os == 'Windows'
         run: |
-          mkdir external
+          mkdir -p external
           curl -L "https://nim-lang.org/download/windeps.zip" -o external/windeps.zip
-          7z x external/windeps.zip -oexternal/dlls
+          7z x -y external/windeps.zip -oexternal/dlls-${{ matrix.target.cpu }}
 
       - name: Path to cached dependencies (Windows)
         if: >
           runner.os == 'Windows'
         run: |
-          echo '${{ github.workspace }}'"/external/dlls" >> $GITHUB_PATH
+          echo "${{ github.workspace }}/external/dlls-${{ matrix.target.cpu }}" >> $GITHUB_PATH
 
       - name: Derive environment variables
         run: |

commit 59bd40e88518d47ef9d5b1262008c595045094fb

avoid double testing `--mm:refc`
Fixes a regression from #69 that results in `--mm:refc` tests running
twice. `run` already tests with both `--mm:orc` and `--mm:refc`.

ssz_serialization.nimble
@@ -40,6 +40,3 @@ proc run(args, path: string) =
 task test, "Run all tests":
   run "--threads:off -d:PREFER_BLST_SHA256=false", "tests/test_all"
   run "--threads:on -d:PREFER_BLST_SHA256=false", "tests/test_all"
-  if (NimMajor, NimMinor) > (1, 6):
-    run "--mm:refc --threads:off -d:PREFER_BLST_SHA256=false", "tests/test_all"
-    run "--mm:refc --threads:on -d:PREFER_BLST_SHA256=false", "tests/test_all"