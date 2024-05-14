5b416b591c45176ad7f9fae2fa90abecec6e125a
Etan Kissling <etan@status.im>
2024-05-14 23:15:45 +0300

    add more test vectors

--- tests/test_ssz_stable_container.nim
+++ tests/test_ssz_stable_container.nim
@@ -367,3 +367,104 @@ suite "SSZ StableContainer":
         circle: ShapeRepr(
           value: ShapePayload(side: 0, color: 1, radius: 0x42),
           active_fields: BitArray[4](bytes: [0b0110'u8]))).hash_tree_root()
+
+  test "StableContainer":
+    type
+      Shape1 {.sszStableContainer: 4.} = object
+        side: Opt[uint16]
+        color: uint8
+        radius: Opt[uint16]
+
+      Shape2 {.sszStableContainer: 8.} = object
+        side: Opt[uint16]
+        color: uint8
+        radius: Opt[uint16]
+
+      Shape3 {.sszStableContainer: 8.} = object
+        side: Opt[uint16]
+        colors: Opt[List[uint8, Limit 4]]
+        radius: Opt[uint16]
+
+    template runTest(value: typed, serialized: string, root: string): untyped =
+      block:
+        let v = value
+        checkpoint $v
+        check:
+          SSZ.encode(v).toHex() == serialized
+          v.hash_tree_root().data.toHex() == root
+
+    runTest(
+      value = Shape1(
+        side: Opt.some 0x42'u16, color: 1, radius: Opt.some 0x42'u16),
+      serialized = "074200014200",
+      root = "37b28eab19bc3e246e55d2e2b2027479454c27ee006d92d4847c84893a162e6d")
+    runTest(
+      value = Shape1(
+        side: Opt.some 0x42'u16, color: 1, radius: Opt.none uint16),
+      serialized = "03420001",
+      root = "bfdb6fda9d02805e640c0f5767b8d1bb9ff4211498a5e2d7c0f36e1b88ce57ff")
+    runTest(
+      value = Shape1(
+        side: Opt.none uint16, color: 1, radius: Opt.none uint16),
+      serialized = "0201",
+      root = "522edd7309c0041b8eb6a218d756af558e9cf4c816441ec7e6eef42dfa47bb98")
+    runTest(
+      value = Shape1(
+        side: Opt.none uint16, color: 1, radius: Opt.some 0x42'u16),
+      serialized = "06014200",
+      root = "f66d2c38c8d2afbd409e86c529dff728e9a4208215ca20ee44e49c3d11e145d8")
+    runTest(
+      value = Shape2(
+        side: Opt.some 0x42'u16, color: 1, radius: Opt.some 0x42'u16),
+      serialized = "074200014200",
+      root = "0792fb509377ee2ff3b953dd9a88eee11ac7566a8df41c6c67a85bc0b53efa4e")
+    runTest(
+      value = Shape2(
+        side: Opt.some 0x42'u16, color: 1, radius: Opt.none uint16),
+      serialized = "03420001",
+      root = "ddc7acd38ae9d6d6788c14bd7635aeb1d7694768d7e00e1795bb6d328ec14f28")
+    runTest(
+      value = Shape2(
+        side: Opt.none uint16, color: 1, radius: Opt.none uint16),
+      serialized = "0201",
+      root = "9893ecf9b68030ff23c667a5f2e4a76538a8e2ab48fd060a524888a66fb938c9")
+    runTest(
+      value = Shape2(
+        side: Opt.none uint16, color: 1, radius: Opt.some 0x42'u16),
+      serialized = "06014200",
+      root = "e823471310312d52aa1135d971a3ed72ba041ade3ec5b5077c17a39d73ab17c5")
+    runTest(
+      value = Shape3(
+        side: Opt.some 0x42'u16,
+        colors: Opt.some List[uint8, Limit 4].init @[1'u8, 2],
+        radius: Opt.some 0x42'u16),
+      serialized = "0742000800000042000102",
+      root = "1093b0f1d88b1b2b458196fa860e0df7a7dc1837fe804b95d664279635cb302f")
+    runTest(
+      value = Shape3(
+        side: Opt.some 0x42'u16,
+        colors: Opt.none List[uint8, Limit 4],
+        radius: Opt.none uint16),
+      serialized = "014200",
+      root = "28df3f1c3eebd92504401b155c5cfe2f01c0604889e46ed3d22a3091dde1371f")
+    runTest(
+      value = Shape3(
+        side: Opt.none uint16,
+        colors: Opt.some List[uint8, Limit 4].init @[1'u8, 2],
+        radius: Opt.none uint16),
+      serialized = "02040000000102",
+      root = "659638368467b2c052ca698fcb65902e9b42ce8e94e1f794dd5296ceac2dec3e")
+    runTest(
+      value = Shape3(
+        side: Opt.none uint16,
+        colors: Opt.none List[uint8, Limit 4],
+        radius: Opt.some 0x42'u16),
+      serialized = "044200",
+      root = "d585dd0561c718bf4c29e4c1bd7d4efd4a5fe3c45942a7f778acb78fd0b2a4d2")
+    runTest(
+      value = Shape3(
+        side: Opt.none uint16,
+        colors: Opt.some List[uint8, Limit 4].init @[1'u8, 2],
+        radius: Opt.some 0x42'u16),
+      serialized = "060600000042000102",
+      root = "00fc0cecc200a415a07372d5d5b8bc7ce49f52504ed3da0336f80a26d811c7bf")
