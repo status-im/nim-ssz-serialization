status-im/nim-ssz-serialization

commit d4c40ded9e57fbe75a7dccda4fc200e412a65a04

Fix IndexError when hashing HashArray|HashList with <= 32byte capacity (#108)
The caches for HashArray|HashList assume a tree size that's a multiple
of 2 chunks, which doesn't work when there is only a single chunk.
Ensure that the single chunk case also works.

ssz_serialization/types.nim
@@ -108,7 +108,7 @@ type
     ## Array implementation that caches the hash of each chunk of data - see
     ## also HashList for more details.
     data*: array[maxLen, T]
-    hashes* {.dontSerialize.}: array[maxChunkIdx(T, maxLen), Digest]
+    hashes* {.dontSerialize.}: array[max(maxChunkIdx(T, maxLen), 2), Digest]
 
   HashList*[T; maxLen: static Limit] = object
     ## List implementation that caches the hash of each chunk of data as well
@@ -135,7 +135,8 @@ type
       ## Flattened tree store that skips "empty" branches of the tree - the
       ## starting index in this sequence of each "level" in the tree is found
       ## in `indices`.
-    indices* {.dontSerialize.}: array[hashListIndicesLen(maxChunkIdx(T, maxLen)), int64] ##\
+    indices* {.dontSerialize.}: array[
+        hashListIndicesLen(max(maxChunkIdx(T, maxLen), 2)), int64] ##\
       ## Holds the starting index in the hashes list for each level of the tree
 
   # Note for readers:
@@ -262,6 +263,7 @@ template clearCache*(v: var Digest) =
 template maxChunks*(a: HashList|HashArray): int64 =
   ## Layer where data is
   const v = maxChunkIdx(a.T, a.maxLen) # force compile-time eval
+  static: doAssert (v mod 2 == 0) or (v == 1)
   v
 
 template maxDepth*(a: HashList|HashArray): int =
@@ -352,7 +354,7 @@ func resizeHashes*(a: var HashList) =
   let
     leaves = int(
       chunkIdx(a, a.data.len() + dataPerChunk(a.T) - 1))
-    newSize = 1 + cacheNodes(a.maxDepth, leaves)
+    newSize = 1 + max(cacheNodes(a.maxDepth, leaves), 1)
 
   # Growing might be because of add(), addDefault(), or in-place reading of a
   # larger HashList. In-place reading of a smaller HashList causes shrinking.
@@ -369,12 +371,12 @@ func resizeHashes*(a: var HashList) =
     assign(newHashes[i], uninitSentinel)
 
   newIndices[0] = nodesAtLayer(0, a.maxDepth, leaves)
-  for i in 1..a.maxDepth:
+  for i in 1 .. max(a.maxDepth, 1):
     newIndices[i] =
       newIndices[i - 1] + nodesAtLayer(i - 1, a.maxDepth, leaves)
 
   # When shrinking, truncate each layer
-  for i in 1 ..< a.maxDepth:
+  for i in 1 ..< max(a.maxDepth, 1):
     for j in 0 ..< min(
         a.indices[i] - a.indices[i-1], newIndices[i] - newIndices[i - 1]):
       newHashes[newIndices[i - 1] + j] = a.hashes[a.indices[i - 1] + j]

ssz_serialization/merkleization.nim
@@ -1020,6 +1020,25 @@ func hashTreeRootAux[T](
     unsupported T
   ok()
 
+func singleDataHash(x: HashArray|HashList, res: var Digest) =
+  static: doAssert x.maxChunks == 1
+  if x.len == 0:
+    res.reset()
+  else:
+    type S = typeof toSszType(declval x.T)
+    when S is BasicType | Digest:
+      when cpuEndian == bigEndian:
+        unsupported typeof(x) # No bigendian support here!
+
+      let
+        bytes = cast[ptr UncheckedArray[byte]](unsafeAddr x.data[0])
+        byteLen = x.data.len * sizeof(x.T)
+        nbytes = min(byteLen, 32)
+      res.data[0 ..< nbytes] = bytes.toOpenArray(0, nbytes - 1)
+      res.data[nbytes ..< 32] = zero64.toOpenArray(nbytes, 31)
+    else:
+      res = hash_tree_root(x.data[0])
+
 func mergedDataHash(x: HashArray|HashList, chunkIdx: int64, res: var Digest) =
   # The merged hash of the data at `chunkIdx` and `chunkIdx + 1`
   mixin hash_tree_root, toSszType
@@ -1078,7 +1097,10 @@ func hashTreeRootCachedPtr*(x: HashArray, vIdx: int64): ptr Digest =
   let px = unsafeAddr x.hashes[vIdx]
   if not isCached(x.hashes[vIdx]):
     # TODO oops. so much for maintaining non-mutability.
-    mergedHash(x, vIdx * 2, px[])
+    when x.maxChunks == 1:
+      singleDataHash(x, px[])
+    else:
+      mergedHash(x, vIdx * 2, px[])
   px
 
 func hashTreeRootCachedPtr*(x: HashList, vIdx: int64): ptr Digest =
@@ -1095,7 +1117,7 @@ func hashTreeRootCachedPtr*(x: HashList, vIdx: int64): ptr Digest =
 
   trs "GETTING ", vIdx, " ", layerIdx, " ", layer, " ", x.indices.len
 
-  doAssert layer < x.maxDepth
+  doAssert layer < x.maxDepth or layer == 0
   if layerIdx >= x.indices[layer + 1]:
     trs "ZERO ", x.indices[layer], " ", x.indices[layer + 1]
     unsafeAddr zeroHashes[x.maxDepth - layer]
@@ -1104,7 +1126,10 @@ func hashTreeRootCachedPtr*(x: HashList, vIdx: int64): ptr Digest =
     if not isCached(px[]):
       trs "REFRESHING ", vIdx, " ", layerIdx, " ", layer
       # TODO oops. so much for maintaining non-mutability.
-      mergedHash(x, vIdx * 2, px[])
+      when x.maxChunks == 1:
+        singleDataHash(x, px[])
+      else:
+        mergedHash(x, vIdx * 2, px[])
     else:
       trs "CACHED ", layerIdx
     px

tests/test_merkleization_types.nim
@@ -60,6 +60,12 @@ type
     x: HashArray[2, E]
     y: HashList[E, 2]
     z: HashList[E, 2]
+    aa: HashArray[2, uint64]
+    ab: HashList[uint64, 2]
+    ac: HashList[uint64, 2]
+    ad: HashArray[1, E]
+    ae: HashList[E, 1]
+    af: HashList[E, 1]
 let
   x = X(
     a: true,
@@ -103,7 +109,13 @@ let
     x: HashArray[2, E](data: [E(x: false, y: true), E(x: true, y: false)]),
     y: HashList[E, 2].init(
       @[E(x: false, y: true), E(x: true, y: false)]),
-    z: HashList[E, 2].init(@[]))
+    z: HashList[E, 2].init(@[]),
+    aa: HashArray[2, uint64](data: [1'u64, 2]),
+    ab: HashList[uint64, 2].init(@[1'u64, 2]),
+    ac: HashList[uint64, 2].init(@[]),
+    ad: HashArray[1, E](data: [E(x: true, y: false)]),
+    ae: HashList[E, 1].init(@[E(x: true, y: false)]),
+    af: HashList[E, 1].init(@[]))
   roots = block:
     var res = {
       # a
@@ -224,14 +236,38 @@ let
       # z
       228: d(0.u256),
       229: d(0.u256),
-      115: d(0'u64)
+      115: d(0'u64),
+
+      # aa
+      58: d([1'u8, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0]),
+
+      # ab
+      118: d([1'u8, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0]),
+      119: d(2'u64),
+
+      # ac
+      120: d([]),
+      121: d(0'u64),
+
+      # ad
+      122: d(1'u8),
+      123: d(0'u8),
+
+      # ae
+      248: d(1'u8),
+      249: d(0'u8),
+      125: d(1'u64),
+
+      # af
+      126: d(0.u256),
+      127: d(0'u64),
     }.toOrderedTable
-    for i in 58 ..< 64:
+    for i in 64 ..< 64:
       res[i] = d(0.u256)
     for i in [
         40, 41, 84, 42, 86, 43, 88, 44, 90, 45, 47, 96, 97, 48,
         196, 197, 98, 49, 100, 50, 102, 51, 52, 53, 216, 217, 108, 109, 54,
-        110, 111, 55, 224, 225, 112, 56, 114, 57]:
+        110, 111, 55, 224, 225, 112, 56, 114, 57, 59, 60, 61, 124, 62, 63]:
       res[i] = d(res.getOrDefault(2 * i + 0), res.getOrDefault(2 * i + 1))
     for i in countdown(31, 1):
       res[i] = d(res.getOrDefault(2 * i + 0), res.getOrDefault(2 * i + 1))

commit 9b1aa53f8c0b6e6cae3bf9036aa7b82d63675c7d

Fix growing from empty HashList (shallow tree)
Loading an empty HashList, then adding the first items incrementally
does not properly resize the cache as the empty HashList and the single
chunk HashList share the same cache hashes.len for shallow trees.

Regression from #108, but that fix is still relevant.
We just have to make sure that the leaves == 0 case is correct as well.

ssz_serialization/types.nim
@@ -537,7 +537,7 @@ func resizeHashes[T, I](
   ## be called whenever `data` shrinks or grows.
   let
     leaves = int(chunkIdx(T, dataLen + dataPerChunk(T) - 1))
-    newSize = 1 + max(cacheNodes(depth, leaves), 1)
+    newSize = 1 + cacheNodes(max(depth, 1), leaves)
 
   # Growing might be because of add(), addDefault(), or in-place reading of a
   # larger HashList. In-place reading of a smaller HashList causes shrinking.

tests/test_hashcache.nim
@@ -128,6 +128,29 @@ suite "HashList mutation":
           hl.mitem(j).y = uint64(1000 + i * 100 + j)
           check hl.hash_tree_root() == hl.data.hash_tree_root()
 
+    test "Add after empty read - " & $maxLen:
+      var hl: HashList[Foo, maxLen]
+      try:
+        readSszBytes(@[], hl)
+      except SszError:
+        raiseAssert "Valid SSZ"
+      for i in 0 ..< int(maxLen):
+        check:
+          hl.add(foo)
+          hl.hash_tree_root() == hl.data.hash_tree_root()
+
+    test "Resize via read - " & $maxLen:
+      var hl: HashList[Foo, maxLen]
+      for i, len in [
+          0, 0, int(maxLen), int(maxLen), 0, 1, 1, int(maxLen), 0, 0, 1]:
+        let contents = (0 ..< len).mapIt(
+          Foo(x: foo.x, y: uint64(1000 * (i + 1) + it)))
+        try:
+          readSszBytes(SSZ.encode(contents), hl)
+        except SszError:
+          raiseAssert "Valid SSZ"
+        check hl.hash_tree_root() == hl.data.hash_tree_root()
+
   runMutationTests(1)
   runMutationTests(2)
   runMutationTests(3)